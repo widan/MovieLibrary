5a9c1f39a06f3e12a1dd8f1cd731a0c11a603ebe
widan <wid.an@wp.pl>
2022-02-26 00:19:29 +0100

    [ML-002] - Added connection to Database

---
+++ MovieLibrary/MovieLibrary.Core/MovieLibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Core
+{
+    public class MovieLibraryContext : DbContext
+    {
+        public MovieLibraryContext(DbContextOptions<MovieLibraryContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
--- MovieLibrary/MovieLibrary/Program.cs
+++ MovieLibrary/MovieLibrary/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MovieLibrary.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MovieLibraryContext>(options =>
+    options.UseSqlServer()
+    );
+
+
 
 var app = builder.Build();
 
